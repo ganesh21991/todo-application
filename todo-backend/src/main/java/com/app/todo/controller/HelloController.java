9c4e9abe50ee06185abfbeec0c09888ef374d1a3
ganesh21991 <ganesh21991@gmail.com>
2023-12-05 13:59:43 +0530

    to-do-app

---
+++ todo-backend/src/main/java/com/app/todo/controller/HelloController.java
@@ -0,0 +1,21 @@
+package com.app.todo.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collections;
+import java.util.Map;
+
+@RestController
+@CrossOrigin(origins =  {"${app.dev.frontend.local}"})
+public class HelloController {
+
+    @GetMapping(value = "/api/message")
+    public Map<String, String> index() {
+        return Collections.singletonMap("message", "Greetings from Spring Boot!");
+    }
+
+}
